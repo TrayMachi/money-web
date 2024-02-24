d55b93e6d10bf094d7cf4b8d26e22786adcd9139
TrayMachi <tristanagraa@gmail.com>
2024-02-25 02:12:43 +0700

    Change navbar behaviour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import { NavBar } from "@/components/nav";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <div className="my-20">
-        <div className="flex justify-center items-center">
+        <div className="flex items-center justify-center">
           <h1>TESTING API COY</h1>
         </div>
         <div>
           <Link
             href={"/addCard"}
-            className="bg-[#94a5df] p-2 rounded-md my-2 mx-[45vw] flex justify-center items-center"
+            className="mx-[45vw] my-2 flex items-center justify-center rounded-md bg-[#94a5df] p-2"
           >
             Add Card
           </Link>
