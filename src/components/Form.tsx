ce7bfd6303971131d38b7ba62ddfc6e23324333b
TrayMachi <tristanagraa@gmail.com>
2024-02-25 02:13:34 +0700

    Reusable form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,35 +1,55 @@
 "use client";
-import React from "react";
-import { type FC } from "react";
+import { type FC, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import LoadingSpinner from "./LoadingSpinner";
 
 interface IField {
   fields: any[];
   btnTitle: string;
+  onSubmit: (e: any) => void;
+  loading: boolean;
 }
 
-const Form: FC<IField> = ({ fields, btnTitle }) => {
+const Form: FC<IField> = ({ fields, btnTitle, onSubmit, loading }) => {
   const pathname = usePathname();
+
+  const [data, setData] = useState<any>(
+    fields
+      ? fields.reduce((acc: any, field: any) => {
+          acc[field.name] = "";
+          return acc;
+        }, {})
+      : {},
+  );
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    onSubmit(data);
+  };
+
   return (
     <div>
-      <form className="mx-auto w-96">
+      <form onSubmit={handleSubmit} className="mx-auto w-96">
         {fields.map((field: any, index: number) => (
           <div
-            key={index + Math.random() * 1564}
+            key={index}
             className="my-4 flex flex-col gap-2"
           >
             <label htmlFor={field.name}>{field.label}</label>
             <input
               type={field.type}
               name={field.name}
+              value={field.value}
+              onChange={(e)=>
+                setData({ ...data, [field.name]: e.target.value })
+              }
               placeholder={field.placeholder}
-              className="border border-slate-300 p-4"
+              className="rounded-lg border border-slate-300 p-4"
             ></input>
           </div>
         ))}
         {["/login", "/register"].includes(pathname) && (
-          <p className="text-end w-full text-sm text-slate-800 font-light italic">
+          <p className="w-full text-end text-sm font-light italic text-slate-800">
             {pathname == "/login" ? (
               <>
                 Not a member?{" "}
@@ -49,8 +69,9 @@ const Form: FC<IField> = ({ fields, btnTitle }) => {
         )}
         <button
           type="submit"
-          className="bg-black text-white w-full mt-4 px-4 py-2 rounded-md hover:bg-gray-800"
+          className="mt-4 flex w-full items-center justify-center rounded-md bg-black px-4 py-2 text-white hover:bg-gray-800"
         >
+          {loading && <LoadingSpinner />}
           {btnTitle}
         </button>
       </form>
