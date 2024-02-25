a00584e5b262fcb428510bea9aded33c4f0eb101
TrayMachi <tristanagraa@gmail.com>
2024-02-26 00:35:33 +0700

    appwrite edit config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_APPWRITE_URL: process.env.NEXT_APPWRITE_URL,
+    NEXT_APPWRITE_PROJECT_ID: process.env.NEXT_APPWRITE_PROJECT_ID,
+    NEXT_APPWRITE_DB_ID: process.env.NEXT_APPWRITE_DB_ID,
+    NEXT_APPWRITE_COLLECTION_ID: process.env.NEXT_APPWRITE_COLLECTION_ID,
+  },
+};
 
 export default nextConfig;
